96dc32e402d746a37e3a8334ab4d40f651968dd5
Marcia Marrocos Amaral <125309272+marcinhanota1000@users.noreply.github.com>
2026-09-20 01:09:14 -0300

    Fix shopping list test to login first and wait for list page before asserting

--- cypress/e2e/web/lista-de-compras.cy.js
+++ cypress/e2e/web/lista-de-compras.cy.js
@@ -1,18 +1,15 @@
 /// <reference types="cypress" />
 
 describe('Funcionalidade: Lista de compras', () => {
-
-    beforeEach(() => {
-        cy.fixture('login').then((dadosLogin) =>{
-            cy.login(dadosLogin.email, dadosLogin.senha)
-        })
+  beforeEach(() => {
+    cy.fixture('login').then((dadosLogin) => {
+      cy.login(dadosLogin.email, dadosLogin.senha);
     });
+  });
 
-    it('Validar entrada na lista de compras', () => {
-        cy.visit('minhaListaDeProdutos')
-        cy.get('h1').should('contain', 'Lista de Compras')
-        cy.url().should('contain', 'minhaListaDeProdutos')
-        
-    });
-    
+  it('Validar entrada na lista de compras', () => {
+    cy.visit('/minhaListaDeProdutos');
+    cy.contains('h1', 'Lista de Compras', { timeout: 20000 }).should('be.visible');
+    cy.url().should('include', '/minhaListaDeProdutos');
+  });
 });
